51d42cd20cbd3c0c2b4dd70df1e185c42df61eae
boidcom <john@boid.com>
2019-02-03 11:22:55 -0600

    added -y to apt install command

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function quitBoid(){
 function installBoid() {
     cmd.get(
         `
-            sudo apt install boinc-client
+            sudo apt install boinc-client -y
             boinccmd --project_attach http://www.worldcommunitygrid.org/ 1061556_a0c611b081f8692b7ef0c11d39e6105c
         `,
         function(err, data, stderr){
